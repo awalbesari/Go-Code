8a2eafeb077037f7f346be295a4fc27eb3228fa5
3274034305980004 <awal@DESKTOP-NADERFO.localdomain>
2022-06-03 11:45:37 +0700

    Create booking-app

--- BOOKING-APP/main.go
+++ BOOKING-APP/main.go
@@ -5,59 +5,87 @@ import (
 	"strings"
 )
 
-func main() {
-	confrenceName := "Go Confrence"
-	const confrenceTickets int = 50
-	var remainingTickets uint = 50
-	bookings := []string{}
+const confrenceTickets int = 50
 
-	fmt.Printf("Welcome to %v booking appplication\n", confrenceName)
-	fmt.Printf("We have total of %v tickets and %v are still available\n", confrenceTickets, remainingTickets)
-	fmt.Println("Get yout tickets here to attend")
+var confrenceName = "Go Confrence"
+var remainingTickets uint = 50
+var bookings = []string{}
+
+func main() {
+
+	greetUser()
 
 	for {
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
-		// ask user for their name
-		fmt.Println("Enter your first name: ")
-		fmt.Scan(&firstName)
 
-		fmt.Println("Enter your last name: ")
-		fmt.Scan(&lastName)
+		firstName, lastName, email, userTickets := getUserInput()
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
 
-		fmt.Println("Enter your email address: ")
-		fmt.Scan(&email)
+		if isValidName && isValidEmail && isValidTicketNumber {
+			bookTicket(userTickets, firstName, lastName, email)
 
-		fmt.Println("Enter number of tickets: ")
-		fmt.Scan(&userTickets)
-
-		if userTickets <= remainingTickets {
-			remainingTickets = remainingTickets - userTickets
-			bookings = append(bookings, firstName+" "+lastName)
-
-			fmt.Printf("Thanks you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, confrenceName)
-
-			firstNames := []string{}
-			for _, booking := range bookings {
-				var names = strings.Fields(booking)
-				firstNames = append(firstNames, names[0])
-			}
-
-			fmt.Printf("The first names of bookings are: %v\n", bookings)
+			firstNames := getFirstNames()
+			fmt.Printf("The first names of booking are: %v\n", firstNames)
 
 			if remainingTickets == 0 {
 				// end program
 				fmt.Println("Our conference is boked our. Come back next year.")
 				break
 			}
-		} else
-		
-		fmt.Printf("We only have %v tickets remaining, so you can't %v tickets\n", remainingTickets, userTickets)
-		continue
+		} else {
+			if !isValidName {
+				fmt.Println("First name or last name you entered is to short")
+			}
+			if !isValidEmail {
+				fmt.Println("Email address you entered doesn't contain @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("number of tickets you entered is invalid")
+			}
+		}
 
 	}
+}
 
+func greetUser() {
+	fmt.Printf("Welcome to %v booking appplication\n", confrenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", confrenceTickets, remainingTickets)
+	fmt.Println("Get yout tickets here to attend")
+}
+
+func getFirstNames() []string {
+	firstNames := []string{}
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+	return firstNames
+}
+
+func getUserInput() (string, string, string, uint) {
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	return firstName, lastName, email, userTickets
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thanks you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, confrenceName)
 }
